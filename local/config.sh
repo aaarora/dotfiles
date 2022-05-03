ebd25cf7313997e8b927d5d5040021de60062e44
aaarora <aaarora@ucsd.edu>
2022-05-03 14:43:36 -0700

    update

---
+++ local/config.sh
@@ -0,0 +1,3 @@
+gsettings set org.gnome.desktop.interface icon-theme "Tela-black"
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode DYNAMIC
